e220937b3cbb257f0f9c5e3e096f9a816a237af4
ensarsnr <dev.anise@outlook.com>
2023-12-31 23:21:52 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
 import Lottie from "lottie-react";
 import computer from "./assets/anims/computer.json";
 import { useTranslation } from "react-i18next";
+import TitleWriter from "./components/TitleWriter";
+import { HiMiniLanguage } from "react-icons/hi2";
+import { useState } from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 function App() {
   const { t, i18n } = useTranslation();
+  const [isTr, setIsTr] = useState(false);
 
-  const clickChangeLang = async (lang) => {
-    await i18n.changeLanguage(lang);
+  const clickChangeLang = async () => {
+    setIsTr(!isTr);
+    await i18n.changeLanguage(isTr ? "tr" : "en");
   };
 
   return (
-    <div className="grid grid-cols-3 h-screen bg-[#092635]">
+    <div className="flex lg:grid lg:grid-cols-3 h-screen bg-[#092635]">
       <div className="lg:col-span-1 lg:block hidden  bg-[#5C8374]">
-        <div class="w-[600px] 2xl:w-[900px]">
+        <div className="w-[600px] 2xl:w-[900px]">
           <Lottie animationData={computer} loop={true} />
         </div>
       </div>
-      <div className=" sm:col-span-2 col-span-3  m-auto text-[#9EC8B9]">
-        <h2 className="text-4xl font-bold mb-5">{t("welcome")} 👋</h2>
-        <h1 className="text-5xl font-bold text-start">Name Surname</h1>
+      <div className="lg:w-3/4 p-5 sm:col-span-2 col-span-3 m-auto text-[#9EC8B9]">
         <div>
-          {/* Burası sürekli değişebilir.. */}
-          <h1 className="text-5xl font-bold mt-5 text-[#5C8374] text-center">
-            Developer
-          </h1>
-        </div>
-        <div className="flex justify-between mt-5">
-          <div>github</div>
-          <div>linkedin</div>
-          <div>selectlang</div>
+          <h2 className="text-4xl font-bold mb-5 text-center">
+            {t("welcome")} 👋
+          </h2>
+          <h1 className="text-5xl font-bold text-center">Name Surname</h1>
+          <div>
+            {/* Burası sürekli değişebilir.. */}
+            <h1 className="text-2xl font-bold mt-5 text-[#5C8374] text-center">
+              <TitleWriter />
+            </h1>
+          </div>
+          <div className="mt-5">
+            <p className="">{t("about")}</p>
+          </div>
+          <div className="flex justify-evenly mt-5">
+            <button>
+              <FaGithub size={30} />
+            </button>
+            <button>
+              <FaLinkedin size={30} />
+            </button>
+            <button onClick={clickChangeLang}>
+              <HiMiniLanguage size={30} />
+              <p className="text-center">{i18n.language}</p>
+            </button>
+          </div>
         </div>
       </div>
     </div>
