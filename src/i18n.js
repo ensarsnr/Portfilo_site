fcd74b62e094af091ac04e3af04234ffbd9f0dda
ensarsnr <dev.anise@outlook.com>
2023-12-31 23:22:22 +0300

    Added about lang

--- src/i18n.js
+++ src/i18n.js
@@ -5,11 +5,18 @@ const resources = {
   tr: {
     translation: {
       welcome: "Hoş geldiniz!",
+      about: `Türkçe lorem ipsum olacak burasıda...`,
     },
   },
   en: {
     translation: {
       welcome: "Welcome!",
+      about: `Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text
+      ever since the 1500s, when an unknown printer took a galley of type
+      and scrambled it to make a type specimen book. It has survived not
+      only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged.`,
     },
   },
 };
