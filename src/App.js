f8081b174a42a106b08402131fc27e3bae5084da
ensarsnr <dev.anise@outlook.com>
2024-01-01 18:13:56 +0300

    New Year New Commit

--- src/App.js
+++ src/App.js
@@ -18,38 +18,34 @@ function App() {
 
   return (
     <div className="flex lg:grid lg:grid-cols-3 h-screen bg-[#092635]">
-      <div className="lg:col-span-1 lg:block hidden  bg-[#5C8374]">
+      <div className="lg:col-span-1 lg:block hidden bg-[#5C8374]">
         <div className="w-[600px] 2xl:w-[900px]">
           <Lottie animationData={computer} loop={true} />
         </div>
       </div>
-      <div className="lg:w-3/4 p-5 sm:col-span-2 col-span-3 m-auto text-[#9EC8B9]">
+      <div className="h-screen flex flex-col justify-evenly lg:w-3/4 p-5 sm:col-span-2 col-span-3 m-auto text-[#9EC8B9]">
         <div>
-          <h2 className="text-4xl font-bold mb-5 text-center">
-            {t("welcome")} 👋
-          </h2>
-          <h1 className="text-5xl font-bold text-center">Name Surname</h1>
+          <h1 className="text-5xl font-bold text-center">Name Surname 👋</h1>
           <div>
-            {/* Burası sürekli değişebilir.. */}
             <h1 className="text-2xl font-bold mt-5 text-[#5C8374] text-center">
               <TitleWriter />
             </h1>
           </div>
-          <div className="mt-5">
-            <p className="">{t("about")}</p>
-          </div>
-          <div className="flex justify-evenly mt-5">
-            <button>
-              <FaGithub size={30} />
-            </button>
-            <button>
-              <FaLinkedin size={30} />
-            </button>
-            <button onClick={clickChangeLang}>
-              <HiMiniLanguage size={30} />
-              <p className="text-center">{i18n.language}</p>
-            </button>
-          </div>
+        </div>
+        <div className="mt-5">
+          <p className="">{t("about")}</p>
+        </div>
+        <div className="flex justify-evenly mt-5">
+          <button>
+            <FaGithub size={60} />
+          </button>
+          <button>
+            <FaLinkedin size={60} />
+          </button>
+          <button onClick={clickChangeLang}>
+            <HiMiniLanguage size={60} />
+            <p className="text-center">{i18n.language}</p>
+          </button>
         </div>
       </div>
     </div>
