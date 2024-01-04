71584d2a790dff9c7d092025cd022179a029b872
ensarsnr <dev.anise@outlook.com>
2024-01-04 23:50:08 +0300

    Deleted TitleWriter component

--- src/components/home/Title.js
+++ src/components/home/Title.js
@@ -4,12 +4,7 @@ import TitleWriter from "./TitleWriter";
 function Title() {
   return (
     <div>
-      <h1 className="text-5xl font-bold text-center">Name Surname 👋</h1>
-      <div>
-        <h1 className="text-2xl font-bold mt-5 text-[#5C8374] text-center">
-          <TitleWriter />
-        </h1>
-      </div>
+      <h1 className="text-5xl font-bold text-start">Name Surname 👋</h1>
     </div>
   );
 }
