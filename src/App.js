2d2959cce31c920206ce509b91280d50148446fd
ensarsnr <dev.anise@outlook.com>
2024-01-05 16:30:00 +0300

    Gallery select added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import GallerySelect from "./view/GallerySelect";
 import Home from "./view/Home";
 import Projects from "./view/Projects";
+
+import TechStack from "./view/TechStack";
 
 function App() {
   return (
     <>
       <Home />
       <Projects />
+      <TechStack />
+      <GallerySelect />
     </>
   );
 }
