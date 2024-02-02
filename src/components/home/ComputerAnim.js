6d29d361c30c74d45b6a2e035e54332e7e45bd5c
ensarsnr <dev.anise@outlook.com>
2024-02-02 16:04:52 +0300

    New image added, deleted lottie

--- src/components/home/ComputerAnim.js
+++ src/components/home/ComputerAnim.js
@@ -1,11 +1,12 @@
 import Lottie from "lottie-react";
 import React from "react";
 import computer from "../../assets/anims/computer.json";
-
+import catDeveloper from "../../assets/img/catDeveloper.png";
 function ComputerAnim() {
   return (
-    <div className="w-[600px] 2xl:w-[900px]">
-      <Lottie animationData={computer} loop={true} />
+    <div className="w-[600px] p-8 2xl:w-[900px]">
+      {/* <Lottie animationData={computer} loop={true} /> */}
+      <img src={catDeveloper} className="rounded-md shadow-xl" />
     </div>
   );
 }
